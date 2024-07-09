81da9c498582c9be07915b57ecf6d5fe2e1f6354
koonlx <ljh1223582317@gmail.com>
2024-07-10 07:09:44 +0900

    Initial commit

---
+++ src/test/java/com/butler/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butler.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
